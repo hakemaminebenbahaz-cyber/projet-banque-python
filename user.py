37d5dee740fcf9abf83526d2004c4aa3ad219aeb
jihadhaliba <jihadoi1999222@gmail.com>
2025-10-13 12:54:40 +0200

    Ajout fonction verifier_login() avec affichage numéro de compte

--- user.py
+++ user.py
@@ -7,3 +7,9 @@ def charger_comptes():
 def sauvegarder_comptes(comptes):
     with open("data/comptes.json", "w") as f:
         json.dump(comptes, f, indent=4)
+def verifier_login(username, password, comptes):
+    if username in comptes and comptes[username]["password"] == password:
+        print(f"Connexion réussie pour {username} - Compte n° {comptes[username]['numero_compte']}")
+        return True
+    return False
+
