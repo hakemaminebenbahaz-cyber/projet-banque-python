3e6cf1613dc1878af51754e37e0106a531dc81d7
hakemaminebenbahaz-cyber <hakemaminebenbahaz@gmail.com>
2025-10-13 12:45:01 +0200

    Ajout constructeur

--- compte.py
+++ compte.py
@@ -1,2 +1,7 @@
 class CompteBancaire:
-    pass
+    def __init__(self, titulaire, solde=2000):
+        self.titulaire = titulaire
+        import random
+        self.account_number = random.randint(1000000000, 9999999999)
+        self.solde = solde
+        self.historique = []
