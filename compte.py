20a7e3994dc1c1c12ed240f43c04f97437fe15e4
hakemaminebenbahaz-cyber <hakemaminebenbahaz@gmail.com>
2025-10-13 12:51:57 +0200

    Ajout des methodes deposer/retrait

--- compte.py
+++ compte.py
@@ -5,3 +5,15 @@ class CompteBancaire:
         self.account_number = random.randint(1000000000, 9999999999)
         self.solde = solde
         self.historique = []
+
+        
+    def deposer(self, montant):
+        self.solde += montant
+        self.historique.append(f"Dépôt : +{montant}")
+
+    def retirer(self, montant):
+        if montant <= self.solde:
+            self.solde -= montant
+            self.historique.append(f"Retrait : -{montant}")
+        else:
+            print(f"Solde insuffisant pour {self.titulaire} !")
