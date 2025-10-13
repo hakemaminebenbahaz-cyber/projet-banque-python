f63ccbb3f773fcf4f596637e2d5d7ec925c89886
hakemaminebenbahaz-cyber <hakemaminebenbahaz@gmail.com>
2025-10-13 12:56:07 +0200

    Ajout des methodes transferer, epargner , afficher_historique

--- compte.py
+++ compte.py
@@ -6,7 +6,7 @@ class CompteBancaire:
         self.solde = solde
         self.historique = []
 
-        
+
     def deposer(self, montant):
         self.solde += montant
         self.historique.append(f"Dépôt : +{montant}")
@@ -17,3 +17,24 @@ class CompteBancaire:
             self.historique.append(f"Retrait : -{montant}")
         else:
             print(f"Solde insuffisant pour {self.titulaire} !")
+    def transferer(self, autre_compte, montant):
+        if montant <= self.solde:
+            self.solde -= montant
+            autre_compte.solde += montant
+            self.historique.append(f"Transfert : -{montant} vers {autre_compte.titulaire}")
+            autre_compte.historique.append(f"Transfert : +{montant} de {self.titulaire}")
+        else:
+            print(f"Solde insuffisant pour {self.titulaire} !")
+
+    def epargner(self, montant):
+        if montant <= self.solde:
+            self.solde -= montant
+            self.epargne += montant
+            self.historique.append(f"Epargne : -{montant}")
+        else:
+            print(f"Solde insuffisant pour {self.titulaire} !")
+
+    def afficher_historique(self):
+        print(f"Historique de {self.titulaire} :")
+        for operation in self.historique:
+            print(operation)
